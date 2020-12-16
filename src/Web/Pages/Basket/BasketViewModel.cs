dfcda058bc338ca6fa831acbe0c6cf4a50e94361
agindt@css.edu <ElliotSmells43!>
2020-12-16 15:59:17 -0600

    Sprint 2 [10 points]  Add a tax calculation and a Grand Total field to each basket in a manner similar to what you did for and order above. (Shows on checkout screen before order is placed now, but after login)

--- src/Web/Pages/Basket/BasketViewModel.cs
+++ src/Web/Pages/Basket/BasketViewModel.cs
@@ -6,6 +6,8 @@ namespace Microsoft.eShopWeb.Web.Pages.Basket
 {
     public class BasketViewModel
     {
+        private const double TAXRATE = .06;
+
         public int Id { get; set; }
         public List<BasketItemViewModel> Items { get; set; } = new List<BasketItemViewModel>();
         public string BuyerId { get; set; }
@@ -13,6 +15,31 @@ namespace Microsoft.eShopWeb.Web.Pages.Basket
         public decimal Total()
         {
             return Math.Round(Items.Sum(x => x.UnitPrice * x.Quantity), 2);
+        }
+
+        public decimal Tax()
+        {
+            var total = 0m;
+            var taxRate = 0m;
+            var tax = 0m;
+
+            total = Total();
+            taxRate = (decimal)TAXRATE;
+            tax = total * taxRate;
+            return tax;
+
+        }
+
+        public decimal GrandTotal()
+        {
+            var total = 0m;
+            var tax = 0m;
+            var grandTotal = 0m;
+
+            total = Total();
+            tax = Tax();
+            grandTotal = total + tax;
+            return grandTotal;
         }
     }
 }
